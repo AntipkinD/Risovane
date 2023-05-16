d5a8b474f80433c5e0cd85ff0792bb6557df1a18
mushroomvoodoo <normeeboy@gmail.com>
2023-05-16 10:41:10 +0300

    meh

--- Risovane/Form1.Designer.cs
+++ Risovane/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ButtonHighlight;
+            pictureBox1.Location = new Point(24, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(400, 400);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(543, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 40);
+            button1.TabIndex = 1;
+            button1.Text = "Диагональ 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(543, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 40);
+            button2.TabIndex = 2;
+            button2.Text = "Диагональ 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(543, 199);
+            button3.Name = "button3";
+            button3.Size = new Size(105, 40);
+            button3.TabIndex = 3;
+            button3.Text = "Горизонталь";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(543, 245);
+            button4.Name = "button4";
+            button4.Size = new Size(105, 40);
+            button4.TabIndex = 4;
+            button4.Text = "Вертикаль";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(543, 291);
+            button5.Name = "button5";
+            button5.Size = new Size(105, 49);
+            button5.TabIndex = 5;
+            button5.Text = "Звездное небо";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(654, 107);
+            button6.Name = "button6";
+            button6.Size = new Size(41, 233);
+            button6.TabIndex = 6;
+            button6.Text = "ОЧИСТИТЬ";
+            button6.TextAlign = ContentAlignment.TopCenter;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- Risovane/Form1.cs
+++ Risovane/Form1.cs
@@ -2,9 +2,91 @@ namespace Risovane
 {
     public partial class Form1 : Form
     {
+        private int width = 400;
+        private int height = 400;
+        private int x = 0;
+        private int y = 0;
+        Random rnd = new Random();
+        int r, g, b;
+        void RndColor()
+        {
+            r = rnd.Next(0, 255);
+            g = rnd.Next(0, 255);
+            b = rnd.Next(0, 255);
+        }
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(width, height);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RndColor();
+            pictureBox1.BackColor = Color.White;
+            y = 0;
+            for (x = 0; x < height; x++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(r, g, b));
+                y++;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RndColor();
+            pictureBox1.BackColor = Color.White;
+            y = width - 1;
+            for (x = 0; x < height; x++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(r, g, b));
+                y--;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RndColor();
+            pictureBox1.BackColor = Color.White;
+            y = width / 2;
+            for (x = 0; x < height; x++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(r, g, b));
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RndColor();
+            pictureBox1.BackColor = Color.White;
+            x = width / 2;
+            for (y = 0; y < height; y++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(r, g, b));
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RndColor();
+            pictureBox1.BackColor = Color.Black;
+            x = rnd.Next(0, 400);
+            y = rnd.Next(0, 400);
+            ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.FromArgb(r, g, b));
+            pictureBox1.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(width, height);
         }
     }
 }
